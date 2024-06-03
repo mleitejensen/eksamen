737a0ba1c18db4a9ad5344a51dc3f2cf7d23f95d
Martin Leite Jensen <marekatt2004@gmail.com>
2024-06-03 22:26:18 +0200

    added env with proxy

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,13 +1,34 @@
-import { useAuthContext } from "../hooks/useAuthContext"
+import { useState } from "react"
+import { useLogin } from "../hooks/useLogin"
 
 const Login = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    //const {login, error, isLoading} = useLogin()
 
-    const { user } = useAuthContext()
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        //await login(username, password)
+    }
 
     return(
-        <div className="login">
-            <h3>Login</h3>
-       </div>
+        <>
+        <form className="loginForm" onSubmit={handleSubmit}>
+        <h2>Sign in and quote with us!</h2>
+            <input type="text" name="username" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)}/> 
+            <br />
+            <input type="password" name="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <br />
+
+            {/* error message here */}
+            <div className="errorBox">
+                {error && <div className="error">{error}</div>}
+            </div>
+
+            <button disabled={isLoading}>Sign in</button>
+        </form>
+        </>
     )
 }
 
