288dd17cde11884f0ac9b8bd4a7f85c9fd09b40f
Martin Leite Jensen <marekatt2004@gmail.com>
2024-06-04 17:45:00 +0200

    Added requireAdmin middleware

---
+++ API/middleware/requireAdmin.js
@@ -0,0 +1,20 @@
+const User = require("../models/userModel")
+
+const requireAdmin = async (req, res) => {
+    try{
+        const user = await User.findById({_id: req.user._id})
+        if(!user.admin){
+            throw Error("Request is not authorized as admin")
+        }
+
+        next()
+
+    }catch(error){
+        console.log(error)
+        res.status(401).json({error: error.message})
+    }
+}
+
+module.exports = {
+    requireAdmin
+}
