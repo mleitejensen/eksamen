e660901e952be50f86cc4fa47b4c31604fdb0372
mleitejens <mleitejensen@gmail.com>
2024-06-04 09:43:17 +0200

    changed font, styled the product post form

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -7,6 +7,31 @@ const Home = () => {
     return(
         <div className="home">
             <h3>Home</h3>
+
+            <form className="postForm">
+                <div className="formLine">
+                    <label htmlFor="name">Apparel name:</label>
+                    <input type="text" name="name" />
+                </div>
+
+                <div className="formLine">
+                    <label htmlFor="url">Apparel img url:</label>
+                    <input type="url" name="url"/>
+                </div>
+
+                <div className="formLine">
+                    <label htmlFor="genre">Sub-genre:</label>
+                    <select name="genre">
+                        <option>T-shirt</option>
+                        <option>Shirt</option>
+                    </select>
+                </div>
+
+                <div className="formTextArea">
+                    <label htmlFor="description">Apparel description:</label>
+                    <textarea name="description" maxLength={100}></textarea>
+                </div>
+            </form>
        </div>
     )
 }
