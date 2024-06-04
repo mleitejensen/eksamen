182e617da981552859cfeb0bb90d317e0e03fa6a
Martin Leite Jensen <marekatt2004@gmail.com>
2024-06-04 19:32:19 +0200

    now shows product on dynamic page

--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -1,16 +1,40 @@
 import { useAuthContext } from "../hooks/useAuthContext"
 import { useParams } from 'react-router-dom';
+import { useGetProduct } from "../hooks/useGetProduct";
+import { useEffect } from "react";
 
 const Product = () => {
     const { user } = useAuthContext()
 
     const {productId} = useParams()
+    const { getProduct, product, isLoading, error } = useGetProduct()
 
+    useEffect(() => {
+        getProduct(productId)
+    },[])
+
+    useEffect(() => {
+        console.log(product)
+    },[product])
 
     return(
         <div className="category">
 
             <h1 className="title">Product Page - {productId}</h1>
+
+            {product && 
+            <>
+                <div className="product">
+                    <img src={product?.image} alt={product.description} />
+                    <h3>{product?.name}</h3>
+                    <p>{product?.description}</p>
+                    <div className="buy">
+                        <button>Add to cart</button>
+                        <input type="number" defaultValue={1}/>
+                    </div>
+                </div>
+            </>
+            }
 
        </div>
     )
