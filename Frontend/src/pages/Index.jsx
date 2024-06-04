718c043eabd840a22b743df3c8aa037ccaf6f6f5
mleitejens <mleitejensen@gmail.com>
2024-06-04 11:07:29 +0200

    Added hook to fetch newest products, index page now shows the newest products

--- Frontend/src/pages/Index.jsx
+++ Frontend/src/pages/Index.jsx
@@ -1,12 +1,38 @@
+import { useEffect } from "react"
 import { useAuthContext } from "../hooks/useAuthContext"
+import { useNewestProducts } from "../hooks/useNewestProducts"
 
 const Index = () => {
-
     const { user } = useAuthContext()
+    const {newestProducts, posts, isLoading, error} = useNewestProducts()
 
+    useEffect(() => {
+        newestProducts()
+    },[])
+
+    useEffect(() => {
+        console.log(posts)
+    },[posts])
     return(
         <div className="index">
-            <h3>Index</h3>
+
+            <div className="productList">
+                {posts?.newest && posts?.newest.map((post) => (
+                    <div key={post?._id} className="gridItem">
+                        <img src={post?.image} alt={post.description} />
+                        <h3>{post?.name}</h3>
+                        <p>{post?.description}</p>
+                    </div>
+                ))}
+                {posts?.newest && posts?.newest.map((post) => (
+                    <div key={post?._id} className="gridItem">
+                        <img src={post?.image} alt={post.description} />
+                        <h3>{post?.name}</h3>
+                        <p>{post?.description}</p>
+                    </div>
+                ))}
+            </div>
+
        </div>
     )
 }
